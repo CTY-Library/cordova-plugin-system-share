88ac31f30fdd03d15682b33b935cb5ff3654c9fb
陈楼乃 <2419538202@qq.com>
2022-08-19 11:31:36 +0800

    img ios

--- www/SystemSharePlugin.js
+++ www/SystemSharePlugin.js
@@ -1,6 +1,14 @@
 var exec = require('cordova/exec');
 
- 
+function AsArray (param) {
+    if (param == null) {
+      param = [];
+    } else if (typeof param === 'string') {
+      param = new Array(param);
+    }
+    return param;
+};
+
 exports.share_txt = function (title,txt,package_name,ui_page, success, error) { 
         exec(success, error, 'SystemSharePlugin', 'share_txt', [title,txt,package_name,ui_page]);    
 };
@@ -16,4 +24,8 @@ exports.share_link = function (title, webpageUrl,package_name,ui_page, success, 
     exec(success, error, 'SystemSharePlugin', 'share_link', [title, webpageUrl, package_name,ui_page]);    
 };
 
- 
+
+exports.share_image_ios = function ( path , success, error) { 
+    exec(success, error, 'SystemSharePlugin', 'share', [null, null, AsArray(path), null, ""]);    
+};
+
